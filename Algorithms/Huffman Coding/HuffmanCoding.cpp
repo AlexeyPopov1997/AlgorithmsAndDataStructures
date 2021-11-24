e731aac9ac4e7e4d9046efea3968690eb93a362f
AlexeyPopov1997 <popov.alexey1997@gamil.com>
2021-11-24 14:23:40 +0300

    1

--- Algorithms/Huffman Coding/HuffmanCoding.cpp
+++ Algorithms/Huffman Coding/HuffmanCoding.cpp
@@ -1,98 +1,133 @@
-#include <iostream>
-#include <unordered_map>
-#include <vector>
 #include <algorithm>
 #include <cassert>
+#include <iostream>
+#include <queue>
+#include <tuple>
+#include <unordered_map>
+#include <vector>
 
-class Huffman {
-    struct CharFrequency
-    {
-        char ch;
+class Huffman{
+    struct CharSetFrequency {
+        std::string charSet;
         int frequency;
 
-        bool operator < (const CharFrequency &other) const {
-            return std::tie(frequency, ch) > std::tie(other.frequency, other.ch);
+        bool operator < (const CharSetFrequency &otherSet) const {
+            return std::tie(frequency, charSet) > std::tie(otherSet.frequency, otherSet.charSet);
         }
     };
 
-    // Input array must be sorted by frequency
-    // Sum of all frequencies must be passed in total_frequency
-    static std::unordered_map<char, std::string> shannon_fano_encode(const std::vector<CharFrequency> &frequencies, int totalFrequency) {
-        size_t size = frequencies.size();
+public:
+    static std::unordered_map<char, std::string> encode(const std::string &inputText) {
+        std::unordered_map<char, int> charFrequencies;
+        for (auto chr : inputText) {
+            charFrequencies[chr]++;
+        }
+
+        std::vector<CharSetFrequency> frequencies;
+        for (auto charFrequency : charFrequencies) {
+            frequencies.push_back({std::string(1, charFrequency.first), charFrequency.second});
+        }
+
+        if (frequencies.size() == 1) {
+            std::unordered_map<char, std::string> result;
+            result[frequencies[0].charSet[0]] = "0";
+            return result;
+        }
+
         std::unordered_map<char, std::string> result;
-        if (size <= 1) {
-            for (auto &frequency:frequencies) {
-                result[frequency.ch] = "";
-            }
-            return std::move(result);
-        }
+        std::priority_queue<CharSetFrequency> queue(frequencies.begin(), frequencies.end());
+        while (queue.size() >= 2) {
+            auto first = queue.top();
+            queue.pop();
 
-        // Greedy split frequencies into two parts with approximately equal total frequency
-        size_t splitPosition;
-        int firstPartFrequency = 0;
-        for (splitPosition = 0; splitPosition < size; splitPosition++) {
-            int currentFrequency = frequencies[splitPosition].frequency;
-            if (firstPartFrequency >= totalFrequency - (firstPartFrequency + currentFrequency)) {
-                // Adding current character increases difference between part frequencies
-                break;
-            }
-            firstPartFrequency += currentFrequency;
-        }
-        assert(splitPosition <= size);
+            auto second = queue.top();
+            queue.pop();
 
-        // Merge part results
-        for (auto &encoded:shannon_fano_encode({frequencies.begin(), frequencies.begin() + splitPosition}, firstPartFrequency)) {
-            result[encoded.first] = "0" +encoded.second;
+            for (auto chr : first.charSet) {
+                result[chr] = "0" + result[chr];
+            }
+
+            for (auto chr : second.charSet) {
+                result[chr] = "1" + result[chr];
+            }
+
+            queue.push({first.charSet + second.charSet, first.frequency + second.frequency});
         }
-        for (auto &encoded:shannon_fano_encode({frequencies.begin() + splitPosition, frequencies.end()}, totalFrequency - firstPartFrequency)) {
-            result[encoded.first] = "1" +encoded.second;
-        }
-        return std::move(result);
+        return result;
     }
 
-    public:
-        static std::unordered_map<char, std::string> encode(const std::string &inputText) {
-            std::unordered_map<char, int> char_frequencies;
-            for (auto ch:inputText) {
-                char_frequencies[ch]++;
-            }
+    static std::string decode(const std::string &inputText, const std::unordered_map<char, std::string> &encoding) {
+        size_t len = inputText.size();
+        size_t position = 0;
+        std::string result;
 
-            std::vector<CharFrequency> frequencies;
-            for (auto char_frequency:char_frequencies) {
-                frequencies.push_back({char_frequency.first, char_frequency.second});
+        while (position < len) {
+            for (auto &encoded : encoding) {
+                if(inputText.substr(position, encoded.second.size()) == encoded.second) {
+                    result += encoded.first;
+                    position += encoded.second.size();
+                    break;
+                }
             }
-            std::sort(frequencies.begin(), frequencies.end());
-
-            if (frequencies.size() == 1) {
-                // Cannot return empty encoding so this case need to be considered separately
-                std::unordered_map<char, std::string> result;
-                result[frequencies[0].ch] = '0';
-                return result;
-            }
-
-            return shannon_fano_encode(std::move(frequencies), static_cast<int>(inputText.size()));
         }
 
-        static std::string decode(const std::string &inputText, const std::unordered_map<char, std::string> &encodedText) {
+        return result;
+    }
 
+    static std::unordered_map<char, int> getEncodingValues(std::string line, std::string delimiter) {
+        std::unordered_map<char, int> encodingValues;
+
+        auto start = 0U;
+        auto end = line.find(delimiter);
+
+        while (end != std::string::npos) {
+            encodingValues[[], []] = line.substr(start, end - start)[0];
+            start = end + delimiter.length();
+            end = line.find(delimiter, start);
         }
+
+        code = stoi(line.substr(start, end));
+        
+        return {letter, code};
+    }
 };
 
-int main() {
+int main(){
+    /*
     std::string inputText;
     std::cin >> inputText;
 
     auto encoding = Huffman::encode(inputText);
 
     std::string encodedText;
-    for (auto ch:inputText) {
-        encodedText += encoding[ch];
+    for (auto chr : inputText) {
+        encodedText += encoding[chr];
     }
-    std::cout << encoding.size() << ' ' << encodedText.size() << std::endl;
-    for (auto &encoded: encoding) {
+
+    std::cout << encoding.size() << " " << encodedText.size() << std::endl;
+    
+    for (auto &encoded : encoding){
         std::cout << encoded.first << ": " << encoded.second << std::endl;
     }
     std::cout << encodedText << std::endl;
+    */
+
+    int letterAmount;
+    int stringLen;
+
+    std::vector<char> letters;
+
+    std::cin >> letterAmount >> stringLen;
+    std::cout << letterAmount << " " << stringLen << std::endl;
+
+    std::string line;
+
+    for (int i = 0; i < letterAmount; i++) {
+        std::cin >> line;
+        char letter;
+        int code;
+        std::tie(letter, code) = Huffman::getEncodingValues(line, ": ");
+    }
 
     return 0;
 }
